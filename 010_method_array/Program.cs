dbde60d15396a8be806d9938dcf8b6e1b6353994
d_ego <dvegor@gmail.com>
2023-09-16 15:06:07 +0400

    add 010_method_array project

---
+++ 010_method_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {71,92,38,44,35,26,75,48};
+
+int n = array.Length;
+int find = 75;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
